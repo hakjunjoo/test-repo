a25bb7865e4cc9783a8679d87c37cc25cf87eb57
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-05-31 16:17:19 +0900

    [level 0] Title: 소인수분해, Time: 1.19 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i=2; i<=n; i++) {
+            while(n%i == 0) {
+                list.add(i);
+                n = n/i;
+                i = 2;
+            }
+        }
+        
+        TreeSet<Integer> list2 = new TreeSet<Integer>(list);
+        ArrayList<Integer> list3 = new ArrayList<Integer>(list2); //list의 중복 제거 과정
+        
+        int[] answer = new int[list3.size()];
+        for(int i=0; i<list3.size(); i++) {
+            answer[i] = list3.get(i);
+        }
+        
+        Arrays.sort(answer);
+        return answer;
+    }
+}
