570405d34126b65ec5867432299ba4d4f0bf8780
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-06-01 20:20:55 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.15 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,15 +1,15 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String answer = "";
         String[] strArr = my_string.split("");
         
         String str = strArr[num1];
         strArr[num1] = strArr[num2];
         strArr[num2] = str;
         
-        for(int i=0; i<strArr.length; i++){
-            answer += strArr[i];
-        }
+        // for(int i=0; i<strArr.length; i++){
+        //     answer += strArr[i];
+        // }
+        String answer = String.join("",strArr);
         
         return answer;
     }
