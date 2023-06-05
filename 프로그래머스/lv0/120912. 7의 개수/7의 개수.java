c71a130d971b3999be545e086a20ac25474bd7ca
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-06-05 12:27:26 +0900

    [level 0] Title: 7의 개수, Time: 11.77 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String str = "";
+        // 배열의 원소 문자열로 바꾸기
+        for(int i=0; i<array.length; i++) {
+            str += array[i] + "";
+        }
+        
+        // 문자열 나누고 7 비교하기
+        for (int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == '7') {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
