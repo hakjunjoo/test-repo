c5409dbf52c5663d298e91e7219cfdd76f0d7840
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-06-08 20:37:59 +0900

    [level 0] Title: 등수 매기기, Time: 0.49 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.java
@@ -12,15 +12,14 @@ class Solution {
         // 등수 매기기
         int[] average = answer.clone();   
         Arrays.sort(average); //정렬
-        for(int i=0; i<average.length-1; i++) {
-            for(int j=i+1; j<average.length; j++) {
-                if(average[i] == average[j]) {
-                    average[j] = average[j];
-                }
-            }
-        }
-        // System.out.println(Arrays.toString(average));
-        Arrays.sort(average); // 다시 정렬
+        // for(int i=0; i<average.length-1; i++) {
+        //     for(int j=i+1; j<average.length; j++) {
+        //         if(average[i] == average[j]) {
+        //             average[j] = average[j];
+        //         }
+        //     }
+        // }
+        System.out.println(Arrays.toString(average));
         int result[] = new int[answer.length];
         for(int i=0; i<answer.length; i++) {
             for(int j=0; j<average.length; j++) {
