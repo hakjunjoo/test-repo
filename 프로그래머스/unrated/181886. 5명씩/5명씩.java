13f2abaa8021b6447bbdc3e8438592ddf780314e
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-06-18 18:08:03 +0900

    [unrated] Title: 5명씩, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] names) {
+        
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<names.length; i+= 5) {
+            list.add(names[i]);
+        }
+        
+        // System.out.println(list.toString());
+        
+        String[] answer = new String[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
