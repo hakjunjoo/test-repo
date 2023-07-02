38319d59b91a9b9f629ed6ffd86e1e18ac095cb5
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-07-02 19:44:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        int temp = x;
+        int sum = 0;
+        
+        while(x>0) {
+            sum += x%10;
+            x = x/10;
+        }
+                
+        if(temp % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
