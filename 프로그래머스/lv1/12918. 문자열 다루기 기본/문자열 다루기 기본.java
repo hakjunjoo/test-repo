a307541fd5d502bf4dc3b04d7e675f113e2b655a
hakjunjoo <128972944+hakjunjoo@users.noreply.github.com>
2023-07-03 20:14:42 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.10 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        
+        if(s.length() == 4 || s.length() == 6) {
+            String str = s.replaceAll("[^0-9]", "");
+            // System.out.println(str);
+            if(str.length() == s.length()) answer = true;
+        }
+        
+        return answer;
+    }
+}
